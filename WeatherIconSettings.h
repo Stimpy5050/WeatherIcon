d0dd65ffc60875618892af7569df61af49fa9fab
Stimpy5050 <dashman@gmail.com>
2009-03-10 03:02:39 +0000

    Enhancing preferences with effects.

--- WeatherIconSettings.h
+++ WeatherIconSettings.h
@@ -3,9 +3,13 @@
 
 @interface WeatherIconSettings : PSListController
 {
+	PSSpecifier* location;
+	PSSpecifier* unit;
 }
 
 -(NSArray*) specifiers;
 -(void)donate:(id) param;
+-(void)showOverride:(id) value specifier:(id) specifier;
+-(void)viewDidBecomeVisible;
 
 @end
--- WeatherIconSettings.mm
+++ WeatherIconSettings.mm
@@ -7,6 +7,42 @@
 
 @implementation WeatherIconSettings
 
+- (void) dealloc
+{
+	[location release];
+	[unit release];
+	[super dealloc];
+}
+
+- (void) showOverride:(id) value specifier:(id) specifier
+{
+	[self setPreferenceValue:value specifier:specifier];
+
+	NSArray* overrideSpecs = [NSArray arrayWithObjects:location, unit, nil];
+	if ([value boolValue])
+	{
+		[self insertContiguousSpecifiers:overrideSpecs afterSpecifierID:@"OverrideLocation" animated:YES];
+	}
+	else
+	{
+		[self removeContiguousSpecifiers:overrideSpecs animated:YES];
+	}
+}
+
+- (void) viewDidBecomeVisible
+{
+	location = [[self specifierForID:@"Location"] retain];
+	unit = [[self specifierForID:@"Celsius"] retain];
+
+	PSSpecifier* override = [self specifierForID:@"OverrideLocation"];
+	id value = [self readPreferenceValue:override];
+	if (![value boolValue])
+	{
+		NSArray* overrideSpecs = [NSArray arrayWithObjects:location, unit, nil];
+		[self removeContiguousSpecifiers:overrideSpecs animated:YES];
+	}
+}
+
 - (NSArray*) specifiers
 {
 	return [self loadSpecifiersFromPlistName:@"Weather Icon" target:self];
