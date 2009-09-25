7f3419a1de5fd83e2dd075ef277d9acee66a9d18
Stimpy5050 <dashman@gmail.com>
2009-09-25 18:59:23 +0000

    Clean up table view

--- WeatherIconPlugin.mm
+++ WeatherIconPlugin.mm
@@ -10,187 +10,65 @@ static NSArray* dayNames = [[NSArray arrayWithObjects:@"SUN", @"MON", @"TUE", @"
 
 static NSString* prefsPath = @"/User/Library/Preferences/com.ashman.WeatherIcon.Condition.plist";
 
-@interface CurrentCell : UITableViewCell
-@property (nonatomic, retain) UIImageView* icon;
-@property (nonatomic, retain) UILabel* temp;
-@property (nonatomic, retain) UILabel* condition;
+@interface ForecastTempView : UIView
+
+@property (nonatomic, retain) NSArray* forecast;
+
 @end
 
-@implementation CurrentCell
+@implementation ForecastTempView
 
-@synthesize icon, temp, condition;
+@synthesize forecast;
 
-- (id)initWithFrame:(CGRect)frame reuseIdentifier:(NSString *)reuseIdentifier
+-(void) drawRect:(struct CGRect) rect
 {
-        self = [super initWithFrame:frame reuseIdentifier:reuseIdentifier];
-        self.backgroundColor = [UIColor blackColor];
-        self.textColor = [UIColor whiteColor];
-        self.hidesAccessoryWhenEditing = YES;
+	NSLog(@"LI:WeatherIcon: Redrawing temps...");
+	int width = (rect.size.width / 5);
 
-        self.icon = [[UIImageView alloc]init];
-        self.icon.backgroundColor = [UIColor blackColor];
+	for (int i = 0; i < self.forecast.count && i < 5; i++)
+	{
+		NSDictionary* day = [self.forecast objectAtIndex:i];
+		
+		NSNumber* daycode = [day objectForKey:@"daycode"];
+		NSString* str = [dayNames objectAtIndex:daycode.intValue];
+        	CGRect r = CGRectMake(rect.origin.x + (width * i), rect.origin.y, 60, 11);
+		[[UIColor whiteColor] set];
+		[str drawInRect:r withFont:[UIFont boldSystemFontOfSize:11] lineBreakMode:UILineBreakModeClip alignment:UITextAlignmentCenter];
 
-        self.temp = [[UILabel alloc]init];
-        self.temp.textAlignment = UITextAlignmentLeft;
-        self.temp.textColor = [UIColor whiteColor];
-        self.temp.font = [UIFont boldSystemFontOfSize:13];
-        self.temp.backgroundColor = [UIColor blackColor];
+		str = [NSString stringWithFormat:@" %@\u00B0", [day objectForKey:@"high"]];
+        	r = CGRectMake(rect.origin.x + (width * i), rect.origin.y + 14, 29, 12);
+		[[UIColor whiteColor] set];
+		[str drawInRect:r withFont:[UIFont boldSystemFontOfSize:12] lineBreakMode:UILineBreakModeClip alignment:UITextAlignmentRight];
 
-        self.condition = [[UILabel alloc]init];
-        self.condition.textAlignment = UITextAlignmentLeft;
-        self.condition.textColor = [UIColor lightGrayColor];
-        self.condition.font = [UIFont boldSystemFontOfSize:11];
-        self.condition.backgroundColor = [UIColor blackColor];
-
-        [self.contentView addSubview:self.icon];
-        [self.contentView addSubview:self.temp];
-        [self.contentView addSubview:self.condition];
-
-        return self;
-}
-
--(void) layoutSubviews
-{
-        [super layoutSubviews];
-
-        CGRect contentRect = self.contentView.bounds;
-        self.icon.frame = CGRectMake(0, 0, 35, 35);
-        self.temp.frame = CGRectMake(35 , 0, contentRect.size.width - 35, 18);
-        self.condition.frame = CGRectMake(35 , 17, contentRect.size.width - 35, 13);
+		str = [NSString stringWithFormat:@" %@\u00B0", [day objectForKey:@"low"]];
+        	r = CGRectMake(rect.origin.x + (width * i) + 31, rect.origin.y + 14, 29, 12);
+		[[UIColor lightGrayColor] set];
+		[str drawInRect:r withFont:[UIFont boldSystemFontOfSize:12] lineBreakMode:UILineBreakModeClip alignment:UITextAlignmentLeft];
+	}
 }
 
 @end
 
-@interface ForecastView : UIView
+@interface ForecastIconView : UIView
 
-@property (nonatomic, retain) UILabel* day;
-@property (nonatomic, retain) UILabel* high;
-@property (nonatomic, retain) UILabel* low;
+@property (nonatomic, retain) NSArray* icons;
 
 @end
 
-@implementation ForecastView
-
-@synthesize high, low, day;
-
-- (id) init
-{
-	self = [super initWithFrame:CGRectMake(0, 0, 60, 35)];
-        self.backgroundColor = [UIColor blackColor];
-
-        self.day = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 60, 11)];
-        self.day.textAlignment = UITextAlignmentCenter;
-        self.day.textColor = [UIColor whiteColor];
-        self.day.font = [UIFont boldSystemFontOfSize:11];
-        self.day.backgroundColor = [UIColor blackColor];
-
-        self.high = [[UILabel alloc] initWithFrame:CGRectMake(0, 14, 29, 12)];
-        self.high.textAlignment = UITextAlignmentRight;
-        self.high.textColor = [UIColor whiteColor];
-        self.high.font = [UIFont boldSystemFontOfSize:12];
-        self.high.backgroundColor = [UIColor blackColor];
-
-        self.low = [[UILabel alloc] initWithFrame:CGRectMake(31, 14, 30, 12)];
-        self.low.textAlignment = UITextAlignmentLeft;
-        self.low.textColor = [UIColor lightGrayColor];
-        self.low.font = [UIFont boldSystemFontOfSize:12];
-        self.low.backgroundColor = [UIColor blackColor];
-
-	[self addSubview:self.day];
-	[self addSubview:self.low];
-	[self addSubview:self.high];
-
-	return self;
-}
-
-@end
-
-@interface ForecastIconCell : UITableViewCell
-
-@property (nonatomic, retain) NSMutableArray* icons;
-
-@end
-
-@implementation ForecastIconCell
+@implementation ForecastIconView
 
 @synthesize icons;
 
-- (id) initWithFrame:(CGRect)frame reuseIdentifier:(NSString *)reuseIdentifier
+-(void) drawRect:(struct CGRect) rect
 {
-        self = [super initWithFrame:frame reuseIdentifier:reuseIdentifier];
-        self.backgroundColor = [UIColor lightGrayColor];
-        self.textColor = [UIColor whiteColor];
-        self.hidesAccessoryWhenEditing = YES;
+	NSLog(@"LI:WeatherIcon: Redrawing icons...");
+	int width = (rect.size.width / 5);
 
-	self.icons = [NSMutableArray arrayWithCapacity:5];
-
-	for (int i = 0; i < 5; i++)
+	for (int i = 0; i < self.icons.count && i < 5; i++)
 	{
-        	UIImageView* v = [[UIImageView alloc] init];
-	        v.backgroundColor = [UIColor blackColor];
-		[self.icons addObject:v];
-		[self.contentView addSubview:v];
-	}
-
-        return self;
-}
-
--(void) layoutSubviews
-{
-        [super layoutSubviews];
-
-        CGRect contentRect = self.contentView.bounds;
-	int width = ((contentRect.size.width - 20) / 5);
-
-	for (int i = 0; i < 5; i++)
-	{
-		UIImageView* v = [self.icons objectAtIndex:i];
-		v.frame = CGRectMake(23 + (width * i), 0, 34, 34);
-	}
-}
-
-@end
-
-@interface ForecastTempCell : UITableViewCell
-
-@property (nonatomic, retain) NSMutableArray* temps;
-
-@end
-
-@implementation ForecastTempCell
-
-@synthesize temps;
-
-- (id) initWithFrame:(CGRect)frame reuseIdentifier:(NSString *)reuseIdentifier
-{
-        self = [super initWithFrame:frame reuseIdentifier:reuseIdentifier];
-        self.backgroundColor = [UIColor whiteColor];
-        self.textColor = [UIColor whiteColor];
-        self.hidesAccessoryWhenEditing = YES;
-
-	self.temps = [NSMutableArray arrayWithCapacity:5];
-
-	for (int i = 0; i < 5; i++)
-	{
-        	ForecastView* v = [[ForecastView alloc] init];
-		[self.temps addObject:v];
-		[self.contentView addSubview:v];
-	}
-
-        return self;
-}
-
--(void) layoutSubviews
-{
-        [super layoutSubviews];
-
-        CGRect contentRect = self.contentView.bounds;
-	int width = ((contentRect.size.width - 20) / 5);
-
-	for (int i = 0; i < 5; i++)
-	{
-		ForecastView* v = [self.temps objectAtIndex:i];
-		v.frame = CGRectMake(10 + (width * i), 0, 34, 34);
+		UIImage* image = [self.icons objectAtIndex:i];
+		CGRect frame = CGRectMake(rect.origin.x + 13 + (width * i), rect.origin.y, 34, 34);
+		[image drawInRect:frame];
 	}
 }
 
@@ -278,52 +156,50 @@ static NSString* prefsPath = @"/User/Library/Preferences/com.ashman.WeatherIcon.
 {
 	NSDictionary* weather = [self.dataCache objectForKey:@"weather"];
 	NSArray* forecast = [weather objectForKey:@"forecast"];
-	switch (indexPath.row)
+	if (indexPath.row == 0)
 	{
-/*
-		case 0:
-        		CurrentCell *current = [tableView dequeueReusableCellWithIdentifier:@"CurrentCell"];
+        	UITableViewCell *fc = [tableView dequeueReusableCellWithIdentifier:@"ForecastIconCell"];
 
-		        if (current == nil)
-		                current = [[[CurrentCell alloc] initWithFrame:CGRectZero reuseIdentifier:@"CurrentCell"] autorelease];
+		if (fc == nil)
+		{
+			fc = [[[UITableViewCell alloc] initWithFrame:CGRectZero reuseIdentifier:@"ForecastIconCell"] autorelease];
+			fc.backgroundColor = [UIColor blackColor];
+				
+			ForecastIconView* fcv = [[[ForecastIconView alloc] initWithFrame:CGRectMake(10, 0, 300, 35)] autorelease];
+			fcv.backgroundColor = [UIColor blackColor];
+			fcv.tag = 42;
+			[fc.contentView addSubview:fcv];
+		}
 
-       			current.temp.text = [NSString stringWithFormat:@"%@: %@\u00B0", [weather objectForKey:@"city"], [weather objectForKey:@"temp"]];;
-			current.condition.text = [weather objectForKey:@"description"];
-			current.icon.image = [self loadIcon:[weather objectForKey:@"icon"]];
-	
-			return current;
-*/
-		case 0:
-        		ForecastIconCell *fc = [tableView dequeueReusableCellWithIdentifier:@"ForecastIconCell"];
+		ForecastIconView* fcv = [fc viewWithTag:42];
+		NSMutableArray* arr = [NSMutableArray arrayWithCapacity:5];
+		for (int i = 0; i < forecast.count && i < 5; i++)
+		{
+			NSDictionary* day = [forecast objectAtIndex:i];
+			[arr addObject:[self loadIcon:[day objectForKey:@"icon"]]];
+		}
+		fcv.icons = arr;
 
-		        if (fc == nil)
-		                fc = [[[ForecastIconCell alloc] initWithFrame:CGRectZero reuseIdentifier:@"ForecastIconCell"] autorelease];
+		return fc;
+	}
+	else
+	{
+        	UITableViewCell *ft = [tableView dequeueReusableCellWithIdentifier:@"ForecastTempCell"];
 
-			for (int i = 0; i < forecast.count && i < 5; i++)
-			{
-				NSDictionary* day = [forecast objectAtIndex:i];
-				UIImageView* v = [fc.icons objectAtIndex:i];
-				v.image = [self loadIcon:[day objectForKey:@"icon"]];
-			}
+		if (ft == nil)
+		{
+			ft = [[[UITableViewCell alloc] initWithFrame:CGRectZero reuseIdentifier:@"ForecastTempCell"] autorelease];
 
-			return fc;
-		default:
-        		ForecastTempCell *ft = [tableView dequeueReusableCellWithIdentifier:@"ForecastTempCell"];
+			ForecastTempView* ftv = [[[ForecastTempView alloc] initWithFrame:CGRectMake(10, 0, 300, 35)] autorelease];
+			ftv.backgroundColor = [UIColor blackColor];
+			ftv.tag = 42;
+			[ft.contentView addSubview:ftv];
+		}
 
-		        if (ft == nil)
-		                ft = [[[ForecastTempCell alloc] initWithFrame:CGRectZero reuseIdentifier:@"ForecastTempCell"] autorelease];
+		ForecastTempView* ftv = [ft viewWithTag:42];
+		ftv.forecast = [forecast copy];
 
-			for (int i = 0; i < forecast.count && i < 5; i++)
-			{
-				NSDictionary* day = [forecast objectAtIndex:i];
-				ForecastView* v = [ft.temps objectAtIndex:i];
-				NSNumber* daycode = [day objectForKey:@"daycode"];
-				v.day.text = [dayNames objectAtIndex:daycode.intValue];
-				v.high.text = [NSString stringWithFormat:@" %@\u00B0", [day objectForKey:@"high"]];
-				v.low.text = [NSString stringWithFormat:@" %@\u00B0", [day objectForKey:@"low"]];
-			}
-
-			return ft;
+		return ft;
         }
 }
 
