1bb7e746f5bf02dd97e6a0ad4aef022f8368a9bf
Stimpy5050 <dashman@gmail.com>
2010-02-08 02:46:50 +0000

    Switch for detail

--- WeatherIconPlugin.mm
+++ WeatherIconPlugin.mm
@@ -6,42 +6,6 @@
 
 static NSString* prefsPath = @"/User/Library/Caches/com.ashman.WeatherIcon.cache.plist";
 
-static NSArray* defaultIcons = [[NSArray arrayWithObjects:
-	@"tstorms", @"tstorms", @"tstorms", @"tstorms", @"tstorms",
-	@"snow", @"snow", @"snow", @"showers", @"showers",
-	@"showers", @"showers", @"showers", @"snow", @"snow",
-	@"snow", @"snow", @"snow", @"snow", @"cloudy",
-	@"cloudy", @"cloudy", @"cloudy", @"cloudy", @"cloudy",
-	@"sunny", @"cloudy", @"cloudy", @"cloudy", @"partly_cloudy",
-	@"partly_cloudy", @"sunny", @"sunny", @"sunny", @"sunny",
-	@"showers", @"sunny", @"tstorms", @"tstorms", @"tstorms",
-	@"showers", @"snow", @"snow", @"snow", @"partly_cloudy",
-	@"tstorms", @"snow", @"tstorms", nil] retain];
-
-static NSArray* defaultNightIcons = [[NSArray arrayWithObjects:
-	@"tstorms", @"tstorms", @"tstorms", @"tstorms", @"tstorms",
-	@"snow", @"snow", @"snow", @"showers", @"showers",
-	@"showers", @"showers", @"showers", @"snow", @"snow",
-	@"snow", @"snow", @"snow", @"snow", @"cloudy",
-	@"cloudy", @"cloudy", @"cloudy", @"cloudy", @"cloudy",
-	@"sunny", @"cloudy", @"cloudy", @"cloudy", @"partly_cloudy_night",
-	@"partly_cloudy_night", @"moon", @"moon", @"moon", @"moon",
-	@"showers", @"sunny", @"tstorms", @"tstorms", @"tstorms",
-	@"showers", @"snow", @"snow", @"snow", @"partly_cloudy_night",
-	@"tstorms", @"snow", @"tstorms", nil] retain];
-
-static NSArray* descriptions = [[NSArray arrayWithObjects:
-	@"Tornado", @"Tropical Storm", @"Hurricane", @"Severe Thunderstorms", @"Thunderstorms",
-	@"Mixed Rain and Snow", @"Mixed Rain and Sleet", @"Mixed Snow and Sleet", @"Freezing Drizzle", @"Drizzle",
-	@"Freezing Rain", @"Showers", @"Showers", @"Snow Flurries", @"Light Snow Showers",
-	@"Blowing Snow", @"Snow", @"Hail", @"Sleet", @"Dust",
-	@"Foggy", @"Haze", @"Smoky", @"Blustery", @"Windy",
-	@"Cold", @"Cloudy", @"Mostly Cloudy", @"Mostly Cloudy", @"Partly Cloudy",
-	@"Partly Cloudy", @"Clear", @"Sunny", @"Fair", @"Fair",
-	@"Mixed Rain and Hail", @"Hot", @"Isolated Thunderstorms", @"Scattered Thunderstorms", @"Scattered Thunderstorms",
-	@"Scattered Showers", @"Heavy Snow", @"Scattered Snow Showers", @"Heavy Snow", @"Partly Cloudy",
-	@"Thunderstorms", @"Snow Showers", @"Isolated Thunderstorms", nil] retain];
-
 @implementation WIForecastView
 
 @synthesize forecast, theme;
@@ -244,6 +208,12 @@ extern "C" UIImage *_UIImageWithName(NSString *);
 
 	if (icon)
 	{
+		NSBundle* wiBundle = [NSBundle bundleWithPath:@"/Library/WeatherIcon"];
+		NSString* wiPath = [wiBundle pathForResource:@"Theme" ofType:@"plist"];
+		if (NSDictionary* theme = [NSDictionary dictionaryWithContentsOfFile:wiPath])
+			if (NSNumber* n = [theme objectForKey:@"StatusBarImageScale"])
+				scale = n.doubleValue;
+
 		NSBundle* bundle = [NSBundle mainBundle];
 		NSString* path = [bundle pathForResource:@"com.ashman.WeatherIcon" ofType:@"plist"];
 		if (NSDictionary* theme = [NSDictionary dictionaryWithContentsOfFile:path])
